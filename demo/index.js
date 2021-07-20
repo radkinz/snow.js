2e6524f17e3a2e92402e2bd185305d298075199c
Ruby Adkins <radkins22@students.stab.org>
2021-07-20 14:26:13 -0400

    change theme of snowfall

--- demo/index.js
+++ demo/index.js
@@ -1,10 +1,10 @@
-import * as Snow from '/demo/node_modules/snowfallz/snow.js'
+import * as Snow from './node_modules/snowfallz/snow.js'
 
 window.onload = function () {
-    console.log(Snow.default)
     //create new snow
     var snow = new Snow.default({
-        id: 'snow'
+        id: 'snow',
+        theme: 'colorful'
     });
     snow.snowfall();
 }
